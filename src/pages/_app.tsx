79ba493373bbcc0ea9284160ce9a4efedb6b8cf0
Rizwan <rizvawn@gmail.com>
2024-09-09 14:19:01 +0200

    clerk authentication added

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,33 @@
 import { GeistSans } from "geist/font/sans";
 import { type AppType } from "next/app";
-
+import {
+  ClerkProvider,
+  SignInButton,
+  SignedIn,
+  SignedOut,
+  UserButton
+} from '@clerk/nextjs'
 import { api } from "~/utils/api";
 
 import "~/styles/globals.css";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
-    <div className={GeistSans.className}>
+    <ClerkProvider>
+      <html lang="en">
+        <body>
+          <SignedOut>
+            <SignInButton />
+          </SignedOut>
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
+          <div className={GeistSans.className}>
       <Component {...pageProps} />
     </div>
+        </body>
+      </html>
+    </ClerkProvider>    
   );
 };
 
